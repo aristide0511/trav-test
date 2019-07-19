63866a82bf2f73e64a72e8938636660d95d007cf
aristide0511 <43759414+aristide0511@users.noreply.github.com>
2019-07-19 19:09:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var gameworld = 'com4';
 var email = 'greghi.fumagalli@gmail.com';
 var password = 'Gregorio1';
 // only change if needed
-var sitter_type = ''; // 'sitter' or 'dual'
+var sitter_type = ''; // 'sitter' or 'duals'
 var sitter_name = ''; // ingame avatar name
 var port = 3000;
 
